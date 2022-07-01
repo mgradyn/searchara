dcc34b162a11880712cdd1dddf536c8688bde02e
mgradyn <mgradyn@gmail.com>
2022-07-01 20:07:03 +0700

    modal v2

--- src/components/characterListModal/characterListModal.jsx
+++ src/components/characterListModal/characterListModal.jsx
@@ -15,6 +15,15 @@ function CharacterListModal({ open, onClose, position }) {
     setTimeout(() => onClose(), 150);
   };
 
+  const [scale, setScale] = useState({
+    minWidth: `250px`,
+    maxHeight: `350px`,
+    height: `350px`,
+    // top: 0,
+    // left: 0,
+    large: false,
+  });
+
   useEffect(() => {
     console.log(position.top);
     if (position.top === 0) {
@@ -22,8 +31,8 @@ function CharacterListModal({ open, onClose, position }) {
         minWidth: `250px`,
         maxHeight: `350px`,
         height: `350px`,
-        top: `calc(100% - 800px)`,
-        left: `calc(50% + 200px )`,
+        // top: `calc(100% - 800px)`,
+        // left: `calc(50% + 200px )`,
         large: false,
       });
     } else {
@@ -31,30 +40,21 @@ function CharacterListModal({ open, onClose, position }) {
         minWidth: `250px`,
         maxHeight: `350px`,
         height: `350px`,
-        top: `calc(${position.top}px - 150px)`,
-        left: position.left,
+        // top: `calc(${position.top}px - 150px)`,
+        // left: position.left,
         large: false,
       });
     }
   }, [position]);
 
-  const [scale, setScale] = useState({
-    minWidth: `250px`,
-    maxHeight: `350px`,
-    height: `350px`,
-    top: 0,
-    left: 0,
-    large: false,
-  });
-
   const toggleScale = () => {
     if (!scale.large) {
       setScale({
-        minWidth: `800px`,
-        maxHeight: `600px`,
-        height: `600px`,
-        top: `calc(100% - 800px)`,
-        left: `calc(50% - 400px )`,
+        minWidth: `80%`,
+        maxHeight: `80%`,
+        height: `80%`,
+        // top: `calc(100% - 800px)`,
+        // left: `calc(50% - 400px )`,
         large: true,
       });
     } else {
@@ -62,8 +62,8 @@ function CharacterListModal({ open, onClose, position }) {
         minWidth: `250px`,
         maxHeight: `350px`,
         height: `350px`,
-        top: `calc(${position.top}px - 150px)`,
-        left: position.left,
+        // top: `calc(${position.top}px - 150px)`,
+        // left: position.left,
         large: false,
       });
     }
@@ -73,65 +73,67 @@ function CharacterListModal({ open, onClose, position }) {
 
   if (!open) return null;
   return ReactDom.createPortal(
-    <div className='container'>
-      <div
-        className='characterListModal appear'
-        ref={ref}
-        style={{
-          top: scale.top,
-          left: scale.left,
-          minWidth: scale.minWidth,
-          maxHeight: scale.maxHeight,
-          height: scale.height,
-        }}
-      >
-        <div className='characterListModal-bar'>
-          <div className='characterListModal-bar-icon icon__button'>
-            <Icon icon='bi:file-earmark' />
+    <div className='container section__padding'>
+      <div className='sub__container overall__max__width'>
+        <div
+          className='characterListModal appear'
+          ref={ref}
+          style={{
+            // top: scale.top,
+            // left: scale.left,
+            minWidth: scale.minWidth,
+            maxHeight: scale.maxHeight,
+            height: scale.height,
+          }}
+        >
+          <div className='characterListModal-bar'>
+            <div className='characterListModal-bar-icon icon__button'>
+              <Icon icon='bi:file-earmark' />
+            </div>
+            <h3 className='title'>Character List</h3>
+            <div className='characterListModal-bar-buttons'>
+              <button
+                className='characterListModal-bar-buttons-scale icon__button'
+                onClick={toggleScale}
+              >
+                <Icon icon='fluent:window-48-regular' />
+              </button>
+              <button
+                className='characterListModal-bar-buttons-exit icon__button'
+                onClick={toggleExit}
+              >
+                <Icon icon='bi:x-lg' />
+              </button>
+            </div>
           </div>
-          <h3 className='title'>Character List</h3>
-          <div className='characterListModal-bar-buttons'>
-            <button
-              className='characterListModal-bar-buttons-scale icon__button'
-              onClick={toggleScale}
-            >
-              <Icon icon='fluent:window-48-regular' />
-            </button>
-            <button
-              className='characterListModal-bar-buttons-exit icon__button'
-              onClick={toggleExit}
-            >
-              <Icon icon='bi:x-lg' />
-            </button>
+          <div className='characterListModal-content'>
+            <div className='characterListModal-content-input__container'>
+              <input
+                type='text'
+                placeholder='Search...'
+                onChange={(e) => {
+                  setSearchTerm(e.target.value);
+                }}
+              />
+            </div>
+            {Object.keys(TARGET_CLASSES)
+              .filter((key) => {
+                if (searchTerm === "") {
+                  return key;
+                } else if (
+                  TARGET_CLASSES[key].name
+                    .toLowerCase()
+                    .includes(searchTerm.toLocaleLowerCase())
+                ) {
+                  return key;
+                }
+              })
+              .map((key) => (
+                <div key={key}>
+                  <p>{TARGET_CLASSES[key].name}</p>
+                </div>
+              ))}
           </div>
-        </div>
-        <div className='characterListModal-content'>
-          <div className='characterListModal-content-input__container'>
-            <input
-              type='text'
-              placeholder='Search...'
-              onChange={(e) => {
-                setSearchTerm(e.target.value);
-              }}
-            />
-          </div>
-          {Object.keys(TARGET_CLASSES)
-            .filter((key) => {
-              if (searchTerm === "") {
-                return key;
-              } else if (
-                TARGET_CLASSES[key].name
-                  .toLowerCase()
-                  .includes(searchTerm.toLocaleLowerCase())
-              ) {
-                return key;
-              }
-            })
-            .map((key) => (
-              <div key={key}>
-                <p>{TARGET_CLASSES[key].name}</p>
-              </div>
-            ))}
         </div>
       </div>
     </div>,
